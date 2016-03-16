a96b7283bcf69cf2554bcb2ff6f40bb6ef3b0414
hafiz703 <mhbt-703@hotmail.com>
2016-03-16 19:30:00 +0800

    Map & Gunner - Hafiz

---
+++ UFOagain/Assets/Hafiz's Stuff/GunnerSprite/rifle/idle/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour {
+	Rigidbody2D myBody;
+	public float moveForce = 200;
+
+
+	// Use this for initialization
+	void Start () {
+
+		myBody = this.GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector2 moveVec = new Vector2 (CrossPlatformInputManager.GetAxis("Horizontal"),CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+		myBody.AddForce (moveVec);
+	}
+}
