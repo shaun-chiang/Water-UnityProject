c83f2c66edbc6aa413760b36f42d5a917add8876
hafiz703 <mhbt-703@hotmail.com>
2016-04-05 22:58:15 +0800

    lavadamage script

---
+++ UFOagain/Assets/LavaDamage.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LavaDamage : MonoBehaviour {
+	bool inLava = false;
+	HealthScript hscript;
+
+	// Use this for initialization
+	void Start () {
+		hscript = GetComponent<HealthScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//OnTriggerEnter ();	
+	
+
+	}
+
+
+
+	void OnTriggerExit(Collider obj){ inLava = false; }
+
+	void OnGUI(){ 
+		if (inLava) {
+			hscript.Damage (1);
+
+			}
+		}
+
+
+	void OnTriggerEnter(Collider obj){
+		if (obj.tag == "Player") {
+			inLava = true;
+		} else {
+			inLava = false;
+		}
+	}
+
+
+
+
+}
